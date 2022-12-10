f633d180e9fc1538f73d08397b372bd1cc5df39c
Lukas Sawon <lukash_savon@mail.ru>
2022-12-10 03:31:44 +0100

    Initial commit - CRA (Custom React App)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+
+export const App = () => (
+	<div>
+		<Header />
+		blah
+		<Footer />
+	</div>
+);
